2f820c168dca4b4257e363871f5fd9ac828fcdea
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-09-18 14:34:18 -0500

    hah whoops

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,7 @@ const FILES_TO_CACHE = [
     "/manifest.webmanifest",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
-],
+];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
